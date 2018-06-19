728225ed238b527b25deaf0f1cc3626d57527192
Andrew Basner <andrewbasner@gmail.com>
2018-06-19 19:22:23 -0400

    Make all setter and getter methods

--- store_item.rb
+++ store_item.rb
@@ -17,8 +17,16 @@ class Dog_leash
     return @length
   end
 
+  def length=(length)
+    @length = length
+  end
+
   def color
     return @color
+  end
+
+  def color=(color)
+    @color = color
   end
 
   def price
@@ -34,10 +42,22 @@ class Dog_leash
   end
 end
 
-lease1 = Dog_leash.new("3m", "black", 12.99)
-p lease1.length
-p lease1.color
-p lease1.price
-lease1.price = 10.99
-p lease1.price
-p lease1.sale_price
+leash1 = Dog_leash.new("3m", "black", 12.99)
+p leash1.price
+leash1.price = 10.99
+p leash1.price
+p leash1.sale_price
+
+leash2 = Dog_leash.new("1m", "pink", 8.99)
+p leash2.length
+leash2.length = "10m"
+p leash2.length
+
+leash3 = Dog_leash.new("5m", "chain", 17.99)
+p leash3.color
+leash3.color = "orange"
+p leash3.color
+
+# dog_leash_1 = {:length => "3m", :color => "black", :price => 12.99}
+# dog_leash_2 = {:length => "1m", :color => "pink", :price => 8.99}
+# dog_leash_3 = {length: "5m", color: "chain", :price => 17.99}
