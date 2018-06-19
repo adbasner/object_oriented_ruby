636fdb99454e954edbe97b161a4671a0fb2e871c
Andrew Basner <andrewbasner@gmail.com>
2018-06-19 19:55:36 -0400

    replace arguments of input with hash using normal and shortcut hash methods

--- store_item.rb
+++ store_item.rb
@@ -6,16 +6,6 @@
 # p "This #{dog_leash_2[:color]} leash is #{dog_leash_2[:length]} and it cost $#{dog_leash_2[:price]}"
 # p "This #{dog_leash_3[:color]} leash is #{dog_leash_3[:length]} and it cost $#{dog_leash_3[:price]}"
 
-class Dog_leash
-  attr_reader :length, :color, :price
-  attr_writer :length, :color, :price
-
-  def initialize(length, color, price)
-    @length = length
-    @color = color
-    @price = price
-  end
-
   # def length
   #   return @length
   # end
@@ -40,6 +30,17 @@ class Dog_leash
   #   @price = price
   # end
 
+class Dog_leash
+  attr_reader :length, :color, :price
+  attr_writer :length, :color, :price
+
+  def initialize(input_options)
+    @length = input_options[:length]
+    @color = input_options[:color]
+    @price = input_options[:price]
+  end
+
+
   def sale_price
     return (@price * 0.90).round(2)
   end
@@ -49,20 +50,20 @@ class Dog_leash
   end
 end
 
-leash1 = Dog_leash.new("3m", "black", 12.99)
+leash1 = Dog_leash.new({:length => "3m", :color => "black", :price => 12.99})
 p leash1.price
 leash1.price = 10.99
 p leash1.price
 p leash1.sale_price
 leash1.info
 
-leash2 = Dog_leash.new("1m", "pink", 8.99)
+leash2 = Dog_leash.new({length: "1m", color: "pink",  price:8.99})
 p leash2.length
 leash2.length = "10m"
 p leash2.length
 leash2.info
 
-leash3 = Dog_leash.new("5m", "chain", 17.99)
+leash3 = Dog_leash.new(length: "5m", color: "chain", price: 17.99)
 p leash3.color
 leash3.color = "orange"
 p leash3.color
