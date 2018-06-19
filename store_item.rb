ac09cef93962497827849f5488e31fd9579895b0
Andrew Basner <andrewbasner@gmail.com>
2018-06-18 21:31:24 -0400

    Add print statements

--- store_item.rb
+++ store_item.rb
@@ -1,3 +1,7 @@
-dog_leash_1 {:length => 3m, :color => black, :price => 12.99}
-dog_leash_2 {:length => 1m, :color => pink, :price => 8.99}
-dog_leash_3 {length: 5m, color: chain, :price => 17.99}
+dog_leash_1 = {:length => "3m", :color => "black", :price => 12.99}
+dog_leash_2 = {:length => "1m", :color => "pink", :price => 8.99}
+dog_leash_3 = {length: "5m", color: "chain", :price => 17.99}
+
+p "This #{dog_leash_1[:color]} leash is #{dog_leash_1[:length]} and it cost $#{dog_leash_1[:price]}"
+p "This #{dog_leash_2[:color]} leash is #{dog_leash_2[:length]} and it cost $#{dog_leash_2[:price]}"
+p "This #{dog_leash_3[:color]} leash is #{dog_leash_3[:length]} and it cost $#{dog_leash_3[:price]}"
