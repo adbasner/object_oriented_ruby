73fb2a0330558fc8f7bafb584e086160622bab9e
Andrew Basner <andrewbasner@gmail.com>
2018-06-19 23:15:34 -0400

    Add class specific methods and super to inheritance example

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,8 +1,8 @@
-class Moving_object
+class MovingObject
   attr_writer :speed, :direction
   attr_reader :speed, :direction
 
-  def initialize
+  def initialize(input_options)
     @speed = 0
     @direction = 'north'
   end
@@ -20,13 +20,30 @@ class Moving_object
   end
 end
 
-class Car < Moving_object
+class Car < MovingObject
+  attr_reader :fuel, :make, :model
+
+  def initialize(input_options)
+    super
+    @fuel = input_options[:fuel]
+    @make = input_options[:make]
+    @model = input_options[:model]
+  end
+
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
-class Bike < Moving_object
+class Bike < MovingObject
+  attr_reader :gear_speed, :type, :weight
+  def initialize(input_options)
+    super
+    @gear_speed = input_options[:gear_speed]
+    @type = input_options[:type]
+    @weight = input_options[:weight]
+  end
+
   def ring_bell
     puts "Ring ring!"
   end
@@ -36,29 +53,36 @@ end
 # Different - ring bell
 
 p "---Car Object---"
-car1 = Car.new
+car1 = Car.new({:fuel => "gasoline", :make => "Ford", :model => "pinto"})
 car1.honk_horn
 
-p car1.speed
-car1.accelerate
-p car1.speed 
-car1.brake
-p car1.speed
+p car1.fuel
+p car1.make
+p car1.model
+# p car1.speed
+# car1.accelerate
+# p car1.speed 
+# car1.brake
+# p car1.speed
 
-p car1.direction
-car1.turn("South")
-p car1.direction
+# p car1.direction
+# car1.turn("South")
+# p car1.direction
 
 p "---Bike Object---"
-bike1 = Bike.new
+bike1 = Bike.new({:gear_speed => "10 speed", :type => "road", :weight => "10 kgs"})
 bike1.ring_bell
 
-p bike1.speed
-bike1.accelerate
-p bike1.speed 
-bike1.brake
-p bike1.speed
+p bike1.type
+p bike1.weight
+p bike1.gear_speed
 
-p bike1.direction
-bike1.turn("South")
-p bike1.direction
+# p bike1.speed
+# bike1.accelerate
+# p bike1.speed 
+# bike1.brake
+# p bike1.speed
+
+# p bike1.direction
+# bike1.turn("South")
+# p bike1.direction
